40bf95073b904fbdfea4e4405305f8f3677e9a2f
alifiaulilalbab <alifiaulilalbab27@gmail.com>
2022-02-15 22:47:48 +0700

    delete website button

--- app/src/main/java/org/d3ifcool/lagaligo/ui/DetailItemActivity.kt
+++ app/src/main/java/org/d3ifcool/lagaligo/ui/DetailItemActivity.kt
@@ -3,7 +3,6 @@ package org.d3ifcool.lagaligo.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import org.d3ifcool.lagaligo.R
@@ -31,9 +30,6 @@ class DetailItemActivity : AppCompatActivity() {
                     dataKebudayaan.namaBudaya.toString(),
                     dataKebudayaan.detailBudaya.toString(), dataKebudayaan.imgBudaya.toString()
                 )
-                binding.btWebsite.setOnClickListener {
-                    openWebsite(dataKebudayaan.web.toString())
-                }
             }
             MainActivity.EXTRA_KULINER -> {
                 val dataKuliner = intent.getSerializableExtra(EXTRA_DATA) as KulinerItem
@@ -41,13 +37,13 @@ class DetailItemActivity : AppCompatActivity() {
                     dataKuliner.namaKuliner.toString(),
                     dataKuliner.detailKuliner.toString(), dataKuliner.imgKuliner.toString()
                 )
-                binding.tvAlamat.text = StringBuilder(getString(R.string.alamat)).append(dataKuliner.alamatKuliner)
-                binding.tvKontak.text = StringBuilder(getString(R.string.kontak)).append(dataKuliner.kontak)
-                binding.btWebsite.visibility = View.GONE
+                binding.tvAlamat.text =
+                    StringBuilder(getString(R.string.alamat)).append(dataKuliner.alamatKuliner)
+                binding.tvKontak.text =
+                    StringBuilder(getString(R.string.kontak)).append(dataKuliner.kontak)
                 binding.btLokasi.setOnClickListener {
                     openWebsite(dataKuliner.lokasiKuliner.toString())
                 }
-                binding.btWebsite.visibility = View.GONE
             }
             MainActivity.EXTRA_WISATA -> {
                 val dataWisata = intent.getSerializableExtra(EXTRA_DATA) as WisataItem
@@ -57,9 +53,6 @@ class DetailItemActivity : AppCompatActivity() {
                 )
                 binding.btLokasi.setOnClickListener {
                     openWebsite(dataWisata.lokasiWisata.toString())
-                }
-                binding.btWebsite.setOnClickListener {
-                    openWebsite(dataWisata.web.toString())
                 }
             }
         }
@@ -72,7 +65,7 @@ class DetailItemActivity : AppCompatActivity() {
         Glide.with(this).load(image).into(binding.imDetail)
     }
 
-    private fun openWebsite(url: String){
+    private fun openWebsite(url: String) {
         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         startActivity(browserIntent)
     }
